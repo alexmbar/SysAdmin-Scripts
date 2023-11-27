efb224dc656c6d12c11477a81442f51dd6f167d4
Alejandro <alexmbar891@gmail.com>
2023-11-27 17:40:58 -0600

    Create FirewallContpaqi.py
    Script para agregar reglas de firewall del uso de programas del contpaqi

---
+++ FirewallContpaqi.py
@@ -0,0 +1,9 @@
+$puertos = @(9079, 9080, 9081, 9082, 9009, 1099, 1139, 1775, 2003, 1139, 9047, 9020, 9005, 23165, 9015, 9012, 1433, 1434, 443, 15443, 7653)
+
+foreach ($puerto in $puertos) {
+    # Permitir tráfico de entrada al puerto
+    New-NetFirewallRule -DisplayName "Contpaqi Inbound $puerto" -Direction Inbound -Protocol TCP -LocalPort $puerto -Action Allow
+
+    # Permitir tráfico de salida al puerto
+    New-NetFirewallRule -DisplayName "Contpaqi Outbound $puerto" -Direction Outbound -Protocol TCP -LocalPort $puerto -Action Allow
+}
