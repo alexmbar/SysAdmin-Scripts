51647db50c2829b973a99ea8b545d83d0bffdd91
Alejandro <alexmbar891@gmail.com>
2024-02-14 10:15:44 -0600

    Update FirewallContpaqi.ps1

--- FirewallContpaqi.ps1
+++ FirewallContpaqi.ps1
@@ -1,3 +1,4 @@
+#powershell -f FirewallContpaqi.ps1
 $puertos = @(9079, 9080, 9081, 9082, 9009, 1099, 1139, 1775, 2003, 1139, 9047, 9020, 9005, 23165, 9015, 9012, 1433, 1434, 443, 15443, 7653)
 
 foreach ($puerto in $puertos) {
